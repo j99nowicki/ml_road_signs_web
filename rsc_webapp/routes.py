9fd6268f3b9c690d9c9ce2730f240b0d13e57bcd
Nowicki <Jakub.Nowicki@vodafone.com>
2020-05-09 20:59:33 +0100

    upload file - inital commit

--- rsc_webapp/routes.py
+++ rsc_webapp/routes.py
@@ -1,7 +1,8 @@
 from rsc_webapp import app
 import json, plotly
-from flask import render_template
+from flask import render_template, request, redirect
 from wrangling_scripts.wrangle_data import return_figures
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +19,16 @@ def index():
     return render_template('index.html',
                            ids=ids,
                            figuresJSON=figuresJSON)
+
+app.config["IMAGE_UPLOADS"] = "static/img/uploads"
+
+@app.route('/upload_image', methods=['GET', 'POST'])
+def upload_image():
+
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.file_name)
+            print("Image saves: " + image.file_name)
+            return redirect(request.url)        
+    return render_template('templates/upload_image.html')
