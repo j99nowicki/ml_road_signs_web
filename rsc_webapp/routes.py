3cba3ca76bc5066c99787ff25d504c5da4aecbd4
Nowicki <Jakub.Nowicki@vodafone.com>
2020-05-10 08:49:46 +0100

    upload file

--- rsc_webapp/routes.py
+++ rsc_webapp/routes.py
@@ -1,13 +1,21 @@
 from rsc_webapp import app
 import json, plotly
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, send_from_directory, url_for
 from wrangling_scripts.wrangle_data import return_figures
 import os
+import logging
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:/workspace/ml/webapp/ml_webapp/rsc_webapp/static/img/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 @app.route('/index')
 def index():
-
     figures = return_figures()
 
     # plot ids for the html id tag
@@ -20,15 +28,43 @@ def index():
                            ids=ids,
                            figuresJSON=figuresJSON)
 
-app.config["IMAGE_UPLOADS"] = "static/img/uploads"
 
-@app.route('/upload_image', methods=['GET', 'POST'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload-image', methods=['GET', 'POST'])
 def upload_image():
+    app.logger.info("/upload-image method:{}, request.files: {}".format(request.method,request.files ))
 
     if request.method == "POST":
+        # check if the post request has the file part
+        if 'image' not in request.files:
+            app.logger.info('No image uploaded')
+            return redirect(request.url)
+        file = request.files['image']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            app.logger.info('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('upload_image.html')
+
+    '''
         if request.files:
             image = request.files["image"]
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.file_name)
-            print("Image saves: " + image.file_name)
+            #image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            image.save('.', image.filename)
+            app.logger.info("Image saved: {}".format(image.filename))
             return redirect(request.url)        
-    return render_template('templates/upload_image.html')
+    return render_template('upload_image.html')
+    '''
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
